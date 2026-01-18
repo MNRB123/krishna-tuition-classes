122c1b888da2787225fb158ee0b9514682390afb
Mukesh <146660144+MNRB123@users.noreply.github.com>
2026-01-18 11:44:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function joinNow() {
+    alert("Welcome to KRISHNA TUITION CLASSES!");
+}
+
+function submitForm() {
+    document.getElementById("success").innerText =
+        "Thank you! Your message has been sent successfully.";
+    return false;
+}
